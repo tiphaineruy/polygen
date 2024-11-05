d25d2a237492324a32f80fe0fe66d8f1882c4ea1
Robert Pasinski <robert.pasinski@callstack.com>
2024-11-05 15:41:49 +0100

    feat: work on export generation

--- packages/wasm-gen/src/api/module.ts
+++ packages/wasm-gen/src/api/module.ts
@@ -1,13 +1,138 @@
 import { readFile } from 'node:fs/promises';
+import * as path from 'node:path';
 // @ts-ignore
 import { decode } from '@webassemblyjs/wasm-parser';
 
-export async function processWasmModule(path: string) {
-  const file = await readFile(path);
-  const parsedModule = decode(file);
-  generateMetadata(parsedModule);
+interface Result {
+  type: string;
+  body: Module[];
 }
 
-function generateMetadata(_module: any) {
-  return {};
+interface Module {
+  type: 'module';
+  id: string;
+  fields: ModuleField[];
+  metadata: any;
+}
+
+type TypeName = 'i32' | 'i64' | 'u32' | 'u64' | 'f32' | 'f64';
+
+type ModuleField = ModuleFunction | ModuleImport | ModuleExport;
+
+interface ModuleFunctionName {
+  type: 'Identifier';
+  value: string;
+  raw: string;
+}
+
+interface ModuleFunctionSignature {
+  type: 'Signature';
+  params: { valtype: TypeName }[];
+  results: TypeName[];
+}
+
+interface ModuleFunction {
+  type: 'Func';
+  name: ModuleFunctionName;
+  signature: ModuleFunctionSignature;
+  body: number[];
+}
+
+type ModuleImportDescriptor = ModuleFuncImportDescriptor;
+
+interface ModuleFuncImportDescriptor {
+  type: 'FuncImportDescr';
+  id: string;
+  signature: ModuleFunctionSignature;
+}
+interface ModuleExportDescriptor {
+  type: 'FuncExportDescr';
+  exportType: string;
+  id: { type: 'NumberLiteral'; value: number; raw: string };
+}
+
+interface ModuleImport {
+  type: 'ModuleImport';
+  module: string;
+  name: string;
+  descr: ModuleImportDescriptor;
+  loc: any;
+}
+
+interface ModuleExport {
+  type: 'ModuleExport';
+  name: string;
+  descr: ModuleExportDescriptor;
+  loc: any;
+}
+
+export class ModuleInfo {
+  private readonly _name: string;
+  private readonly _escapedName: string;
+  private readonly _fields: ModuleField[];
+  private readonly _funcs: Map<string, ModuleFunctionSignature> = new Map();
+
+  constructor(name: string, metadata: Result) {
+    this._name = name;
+    this._escapedName = escapeModuleName(name);
+    console.log('escaped name: ', this._escapedName);
+    this._fields = metadata.body[0]?.fields ?? [];
+
+    // Build map of funcs
+    for (const field of this._fields) {
+      if (field.type === 'Func') {
+        this._funcs.set(field.name.value, field.signature);
+      }
+    }
+  }
+
+  static async fromWASM(wasmPath: string): Promise<ModuleInfo> {
+    const file = await readFile(wasmPath);
+    const name = path.basename(wasmPath, '.wasm');
+    const decodeOptions = { ignoreCodeSection: true, ignoreDataSection: true };
+    const parsedModule = decode(file, decodeOptions) as Result;
+    return new ModuleInfo(name, parsedModule);
+  }
+
+  public get exports(): (any | null)[] {
+    const exportFields = this._fields.filter((a) => a.type === 'ModuleExport');
+    return exportFields
+      .map((ex) => {
+        if (ex.descr.exportType === 'Func') {
+          const sig = this._funcs.get(`func_${ex.descr.id.raw}`);
+
+          if (!sig) {
+            console.warn(`Could not find export signature for: ${ex.name}`);
+            return null;
+          }
+
+          return {
+            name: ex.name,
+            type: 'func',
+            generated_c_func_name: `w2c_${this._escapedName}_${escapeExportName(ex.name)}`,
+            params: sig.params.map((e) => e.valtype),
+            results: sig.results,
+          };
+        }
+
+        console.warn('Found unknown export type: ', ex.descr.exportType);
+        return null;
+      })
+      .filter((e) => e !== null);
+  }
+}
+
+function escapeNonAsciiChar(c: string) {
+  return '0x' + (c.codePointAt(0) ?? 0).toString(16).toUpperCase();
+}
+
+function escapeModuleName(name: string): string {
+  name = name.replace(/_/g, '__');
+  name = name.replace(/[^[a-zA-Z0-9_]/, escapeNonAsciiChar);
+  return name;
+}
+
+function escapeExportName(name: string): string {
+  name = name.replace(/^_(?=_)/, escapeNonAsciiChar);
+  return name;
 }
--- packages/wasm-gen/src/commands/generate.ts
+++ packages/wasm-gen/src/commands/generate.ts
@@ -3,29 +3,31 @@ import { fileURLToPath } from 'node:url';
 import { Command } from 'commander';
 import consola from 'consola';
 import { glob } from 'glob';
-import { execa } from 'execa';
+import { oraPromise } from 'ora';
+// import { execa } from 'execa';
 import fs from 'node:fs/promises';
 import chalk from 'chalk';
 import path from 'path';
 import { Liquid } from 'liquidjs';
 import { findProjectRoot } from '../api/project.js';
+import { ModuleInfo } from '../api/module.js';
 
 const command = new Command('generate').description(
   'Generates React Native Modules from Wasm'
 );
 
 const ROOT_DIR = path.join(fileURLToPath(import.meta.url), '../../..');
-const ASSETS_DIR = path.join(ROOT_DIR, 'assets');
+// const ASSETS_DIR = path.join(ROOT_DIR, 'assets');
 const TEMPLATES_DIR = path.join(ROOT_DIR, 'templates');
 
 command.action(async () => {
-  const waToolkitPath = process.env.WABT_PATH;
-  if (!waToolkitPath) {
-    consola.error(
-      'WABT_PATH environment variable is not set. Set it to a directory containing WABT toolkit'
-    );
-    return;
-  }
+  // const waToolkitPath = process.env.WABT_PATH;
+  // if (!waToolkitPath) {
+  //   consola.error(
+  //     'WABT_PATH environment variable is not set. Set it to a directory containing WABT toolkit'
+  //   );
+  //   return;
+  // }
 
   const engine = new Liquid({
     root: TEMPLATES_DIR,
@@ -35,35 +37,74 @@ command.action(async () => {
   const generatedDir = path.join(projectRoot, 'wasm/_generated');
 
   const modules = await glob('wasm/*.wasm', { cwd: projectRoot });
-  const wasm2c = path.join(waToolkitPath, 'wasm2c');
-  const pathToRuntimeHeader = path.join(ASSETS_DIR, 'wasm-rt-weak.h');
+  // const wasm2c = path.join(waToolkitPath, 'wasm2c');
+  // const pathToRuntimeHeader = path.join(ASSETS_DIR, 'wasm-rt-weak.h');
 
-  const hostOutputDir = path.join(generatedDir, `@rn-wasm-host`);
-  await fs.mkdir(hostOutputDir, { recursive: true });
+  // const hostOutputDir = path.join(generatedDir, `@rn-wasm-host`);
+  // await fs.mkdir(hostOutputDir, { recursive: true });
+  await fs.mkdir(generatedDir, { recursive: true });
 
   for (const mod of modules) {
     console.log('Processing WebAssembly module: ', chalk.bold(mod));
     const name = path.basename(mod, '.wasm');
-    const fullInPath = path.join(projectRoot, mod);
+    // const fullInPath = path.join(projectRoot, mod);
 
-    const libOutputDir = path.join(generatedDir, name);
-    await fs.mkdir(libOutputDir, { recursive: true });
+    // const libOutputDir = path.join(generatedDir, name);
+    // await fs.mkdir(libOutputDir, { recursive: true });
 
-    const fullOutPath = path.join(libOutputDir, `${name}.c`);
-    await execa(wasm2c, [fullInPath, '-o', fullOutPath]);
-    await fs.copyFile(
-      pathToRuntimeHeader,
-      path.join(libOutputDir, 'wasm-rt.h')
+    // const fullOutPath = path.join(libOutputDir, `${name}.c`);
+    // await execa(wasm2c, [fullInPath, '-o', fullOutPath]);
+    // await fs.copyFile(
+    //   pathToRuntimeHeader,
+    //   path.join(libOutputDir, 'wasm-rt.h')
+    // );
+
+    const parsed = await oraPromise(
+      ModuleInfo.fromWASM(mod),
+      `Loading ${chalk.bold(mod)} metadata`
     );
 
-    const buildFile = await engine.renderFile('BUILD.bazel.liquid', {
-      name,
-    });
-    await fs.writeFile(path.join(libOutputDir, 'BUILD'), buildFile);
-    await fs.copyFile(
-      path.join(ASSETS_DIR, 'Info.plist'),
-      path.join(libOutputDir, 'Info.plist')
+    await fs.writeFile(
+      path.join(generatedDir, `${name}.exports.json`),
+      JSON.stringify(parsed.exports, null, 2)
     );
+
+    const exports = parsed.exports;
+
+    await oraPromise(async () => {
+      const ctx = {
+        moduleSafeName: 'rapier',
+        exports,
+      };
+      const generatedCpp = await engine.renderFile(
+        'exports-jsi-bridge.cpp.liquid',
+        ctx
+      );
+      await fs.writeFile(
+        path.join(generatedDir, 'jsi-exports-bridge.cpp'),
+        generatedCpp
+      );
+      const generatedH = await engine.renderFile(
+        'exports-jsi-bridge.h.liquid',
+        ctx
+      );
+      await fs.writeFile(
+        path.join(generatedDir, 'jsi-exports-bridge.h'),
+        generatedH
+      );
+    }, 'Generating C bridge');
+
+    consola.info('Generated', exports.length, 'exports');
+    // console.log('Parsed: ', parsed.body[0].fields[3]);
+
+    // const buildFile = await engine.renderFile('BUILD.bazel.liquid', {
+    //   name,
+    // });
+    // await fs.writeFile(path.join(libOutputDir, 'BUILD'), buildFile);
+    // await fs.copyFile(
+    //   path.join(ASSETS_DIR, 'Info.plist'),
+    //   path.join(libOutputDir, 'Info.plist')
+    // );
   }
 });
 
