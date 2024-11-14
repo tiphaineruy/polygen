f4a96b71422c844080af4dfc7f36e8d538590412
Robert Pasinski <robert.pasinski@callstack.com>
2024-11-14 18:46:05 +0100

    feat: update example app code, changed WASM module to compute fibonacci using an imported function

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -1,34 +1,74 @@
 import WebAssembly from '../../../packages/react-native-wasm/src/index';
 // import { moduleRef } from 'react-native-wasm/react-native';
-import { useState, useCallback } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useState, useCallback, useMemo } from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+
+const imports = {
+  host: {
+    add: (a: number, b: number) => a + b,
+  },
+};
 
 export default function App() {
   const [module, setModule] = useState<any>();
   const [instance, setInstance] = useState<any>();
+  const [number, setNumber] = useState<number>(0);
+  const [result, setResult] = useState<number>();
+
+  const numberText = useMemo(() => number.toString(), [number]);
 
   const loadModule = useCallback(async () => {
     setModule(await WebAssembly.compile(WebAssembly.moduleRef('example')));
   }, []);
 
   const makeInstance = useCallback(async () => {
-    setInstance(await WebAssembly.instantiate(module!));
+    setInstance(await WebAssembly.instantiate(module!, imports));
   }, [module]);
+
+  const onNumberChanged = useCallback(
+    (value: string) => {
+      const parsed = parseInt(value.replace(/ /g, ''), 10);
+      setNumber(isNaN(parsed) ? 0 : parsed);
+      setResult(undefined);
+    },
+    [setNumber, setResult]
+  );
+
+  const compute = useCallback(() => {
+    // console.log('fib(2):');
+    setResult(instance.exports.fib(number));
+  }, [number, instance, setResult]);
 
   return (
     <View style={styles.container}>
-      <Button title="Load module" onPress={loadModule} />
+      <Button title="Load module" onPress={loadModule} disabled={!!module} />
+      <Text>Module Loaded: {module != null ? 'yes' : 'no'}</Text>
       <Button
         title="Create instance"
         disabled={!module}
         onPress={makeInstance}
       />
-      <Text>Module Loaded: {module != null}</Text>
-      <Text>Instance created: {instance != null}</Text>
+      {/*{module != null && (*/}
+      {/*  <>*/}
+      {/*    <Text>Imports: {JSON.stringify(WebAssembly.Module.imports(module))}</Text>*/}
+      {/*    <Text>Exports: {JSON.stringify(WebAssembly.Module.exports(module))}</Text>*/}
+      {/*  </>*/}
+      {/*)}*/}
+      <Text>Instance created: {instance != null ? 'yes' : 'no'}</Text>
       {instance != null && (
         <Text>Exports: {JSON.stringify(instance.exports)}</Text>
       )}
-      {/*<Text>Result: {result}</Text>*/}
+      <TextInput
+        style={styles.input}
+        value={numberText}
+        onChangeText={onNumberChanged}
+        editable={!!instance}
+        aria-disabled={!instance}
+      />
+      <Button title="Compute" onPress={compute} disabled={!instance} />
+      <Text>
+        fib({number}) = {result ?? '---'}
+      </Text>
     </View>
   );
 }
@@ -43,5 +83,11 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
